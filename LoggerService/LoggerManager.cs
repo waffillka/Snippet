09449db32d4e9e739ecef2978dd0415d37fff8fb
“Sebik <waffilka@gmail.comSebik Georgegit config --global user.email waffilka@gmail.com>
2021-07-25 19:42:59 +0300

    Added ILoggerManager and implementation LoggerManager

---
+++ LoggerService/LoggerManager.cs
@@ -0,0 +1,36 @@
+﻿using Contracts.LoggerService;
+//using Microsoft.Extensions.Logging;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerService
+{
+    class LoggerManager: ILoggerManager
+    {
+        private static ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        public void LogDebug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void LogError(string message)
+        {
+            _logger.Error(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            _logger.Info(message);
+        }
+
+        public void LogWarn(string message)
+        {
+            _logger.Warn(message);
+        }
+    }
+}
