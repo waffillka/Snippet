7a1c4e384191c0cd6a0a0be00e7c792565649fb3
“Sebik <waffilka@gmail.comSebik Georgegit config --global user.email waffilka@gmail.com>
2021-07-25 20:58:52 +0300

    added logger configuration

---
+++ SnippetProject/Extensions/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Contracts.LoggerService;
+using LoggerService;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SnippetProject.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+    }
+}
