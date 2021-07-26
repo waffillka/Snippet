77a997f1209ae30cbacfda01c649081f377ea8f2
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-07-26 10:32:59 +0300

    added RepositoryContext for database models

--- Entities/Models/SnippetPost.cs
+++ Entities/Models/SnippetPost.cs
@@ -1,20 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entities.Models
 {
     public class SnippetPost
     {
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Title is a required field.")]
+        [MaxLength(140, ErrorMessage = "Maximum length for the Title is 140 characters.")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description is a required field.")]
+        [MaxLength(2000, ErrorMessage = "Maximum length for the Description is 2000 characters.")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Snippet is a required field.")]
+        [MaxLength(2000, ErrorMessage = "Maximum length for the Snippet is 2000 characters.")]
         public string Snippet { get; set; }
         public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "LanguageId is a required field.")]
+        [ForeignKey(nameof(Language))]
         public Guid LanguageId { get; set; }
         public Language Language { get; set; }
+
+        [Required(ErrorMessage = "UserId is a required field.")]
+        [ForeignKey(nameof(User))]
         public Guid UserId { get; set; }
         public User User { get; set; }
 
---
+++ Entities/RepositoryContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<SnippetPost> SnippetPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
