d9cfeff02da222300f5f0bf46a2693051028e267
ZhavoronkovM <zhavoronkov.02@mail.ru>
2021-07-28 15:43:15 +0300

    Add FilterRequest model.

---
+++ ServicesProviders/FilterRequest/FilterRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ServicesProviders.FilterRequest
+{
+    public class FilterRequest
+    {
+        public List<ulong>? Authors { get; set; }
+        public List<ulong>? AuthorsExclude { get; set; }
+        public List<ulong>? Tags { get; set; }
+        public List<ulong>? TagsExclude { get; set; }
+        public List<ulong>? Langs { get; set; }
+        public List<ulong>? LangsExclude { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+    }
+}
