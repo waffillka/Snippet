3ee1ef948a56972ee1544dc2233413cd64d18e8d
UlyanYunakh <ulyanyunakh@gmail.com>
2021-07-28 20:37:27 +0300

    ADd Date and Langs predicates.

---
+++ ServicesProviders/SnippetPredicateBuilder/LangsPredicateBuilder.cs
@@ -0,0 +1,32 @@
+using Entities.Models;
+using LinqKit;
+using ServicesProviders.RequestModel;
+
+namespace ServicesProviders.SnippetPredicateBuilder
+{
+    internal class LangsPredicateBuiler : IPredicateBuilder
+    {
+        private readonly IPredicateBuilder _wrappedBuilder;
+        public LangsPredicateBuiler(IPredicateBuilder wrappedBuilder)
+        {
+            _wrappedBuilder = wrappedBuilder;
+        }
+
+        public ExpressionStarter<SnippetPost> BuildPredicate(FilterRequest filterRequest)
+        {
+            var predicate = PredicateBuilder.New<SnippetPost>().And(snippet => true);
+            
+            if(filterRequest.Langs != null)
+            {
+                predicate.And(snippet => filterRequest.Langs.Contains(snippet.Id));
+            }
+            if(filterRequest.LangsExclude != null)
+            {
+                predicate.And(snippet => !filterRequest.LangsExclude.Contains(snippet.Id));
+            }
+
+            return _wrappedBuilder.BuildPredicate(filterRequest).And(predicate);
+        }
+
+    }
+}
