199a8544c3e629e6286b8790e040c7f19e5ce105
ZhavoronkovM <zhavoronkov.02@mail.ru>
2021-07-28 23:20:13 +0300

    Add new predicate builders.

---
+++ ServicesProviders/SnippetPredicateBuilder/AuthorPredicateBuilder.cs
@@ -0,0 +1,33 @@
+﻿using Entities.Models;
+using LinqKit;
+using ServicesProviders.RequestModel;
+
+namespace ServicesProviders.SnippetPredicateBuilder
+{
+    internal class AuthorPredicateBuilder : IPredicateBuilder
+    {
+        private IPredicateBuilder _wrappedBuilder;
+
+        public AuthorPredicateBuilder(IPredicateBuilder wrappedBuilder)
+        {
+            _wrappedBuilder = wrappedBuilder;
+        }
+        public ExpressionStarter<SnippetPost> BuildPredicate(FilterRequest filterRequest)
+        {
+            var predicate = PredicateBuilder.New<SnippetPost>().And(x => true);
+
+            //what if it's null?
+            if (filterRequest.Authors != null)
+            {
+                predicate.And(x => filterRequest.Authors.Contains(x.Id));
+            }
+
+            if (filterRequest.AuthorsExclude != null)
+            {
+                predicate.And(x => !filterRequest.AuthorsExclude.Contains(x.Id));
+            }
+            
+            return _wrappedBuilder.BuildPredicate(filterRequest).And(predicate);
+        }
+    }
+}
