d082b3c1eac863a77c46c939881c539367955be3
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-07-30 22:33:17 +0300

    fix response models

--- Snippet.Services/Models/SnippetPost.cs
+++ Snippet.Services/Models/SnippetPost.cs
@@ -9,28 +9,22 @@ namespace Snippet.Services.Models
         public ulong Id { get; set; }
 
         [Required(ErrorMessage = "Title is a required field.")]
-        [MaxLength(140, ErrorMessage = "Maximum length for the Title is 140 characters.")]
-        public string Title { get; set; } = string.Empty;
+        public string Title { get; set; }
 
         [Required(ErrorMessage = "Description is a required field.")]
-        [MaxLength(2000, ErrorMessage = "Maximum length for the Description is 2000 characters.")]
-        public string Description { get; set; } = string.Empty;
+        public string Description { get; set; }
 
         [Required(ErrorMessage = "Snippet is a required field.")]
-        [MaxLength(2000, ErrorMessage = "Maximum length for the Snippet is 2000 characters.")]
-        public string Snippet { get; set; } = string.Empty;
-        public DateTime Date { get; set; }
+        public string Snippet { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
 
         [Required(ErrorMessage = "LanguageId is a required field.")]
         public ulong LanguageId { get; set; }
 
-        public Language Language { get; set; }
-
         [Required(ErrorMessage = "UserId is a required field.")]
         public ulong UserId { get; set; }
 
-        public User? User { get; set; }
-        public int Like { get; set; }
-        public ICollection<Tag> Tags { get; set; }
+        public ICollection<ulong>? Tags { get; set; }
     }
 }
