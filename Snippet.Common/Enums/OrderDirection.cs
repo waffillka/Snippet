59346551d17b1fbca4a2207989ac6e8d86d69089
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-07-30 14:41:20 +0300

    add enum for order's direction

--- Snippet.Common/Enums/OrderDirection.cs
+++ Snippet.Common/Enums/OrderDirection.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Snippet.Common.Enums
+﻿namespace Snippet.Common.Enums
 {
     public enum OrderDirection
     {
