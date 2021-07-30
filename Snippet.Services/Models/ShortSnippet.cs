01cc2ba9332a6dd1ab033d85bb384f02e8919cc0
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-07-30 22:33:17 +0300

    fix response models

---
+++ Snippet.Services/Models/ShortSnippet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Snippet.Services.Models
+{
+    public class ShortSnippetPost
+    {
+        public ulong Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public Language Language { get; set; }
+
+        public User User { get; set; }
+
+        public int Like { get; set; }
+        public ICollection<Tag>? Tags { get; set; }
+    }
+}
