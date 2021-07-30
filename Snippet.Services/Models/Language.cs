17889817babd129667ef856ed253b9161dde6e3c
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-07-30 21:37:38 +0300

    Create response models

---
+++ Snippet.Services/Models/Language.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Snippet.Services.Models
+{
+    public class Language
+    {
+        public ulong Id { get; set; }
+        [Required(ErrorMessage = "Language name is a required field.")]
+        public string Name { get; set; }
+    }
+}
--- Snippet.Services/Models/SnippetShortPost.cs
+++ Snippet.Services/Models/SnippetShortPost.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Snippet.Services.Models
 {
-    public class SnippetPost
+    public class SnippetShortPost
     {
         public ulong Id { get; set; }
 
@@ -15,10 +15,6 @@ namespace Snippet.Services.Models
         [Required(ErrorMessage = "Description is a required field.")]
         [MaxLength(2000, ErrorMessage = "Maximum length for the Description is 2000 characters.")]
         public string Description { get; set; } = string.Empty;
-
-        [Required(ErrorMessage = "Snippet is a required field.")]
-        [MaxLength(2000, ErrorMessage = "Maximum length for the Snippet is 2000 characters.")]
-        public string Snippet { get; set; } = string.Empty;
         public DateTime Date { get; set; }
 
         [Required(ErrorMessage = "LanguageId is a required field.")]
@@ -30,6 +26,7 @@ namespace Snippet.Services.Models
         public ulong UserId { get; set; }
 
         public User? User { get; set; }
+
         public int Like { get; set; }
         public ICollection<Tag> Tags { get; set; }
     }
