666e640ce19b01a13ab3e5264d55366be1be1cbd
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-07-31 21:57:39 +0300

    added snippet service

---
+++ Snippet.Services/Services/SnippetSevice.cs
@@ -0,0 +1,30 @@
+﻿using Snippet.Services.Interfaces.Service;
+using Snippet.Services.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snippet.Services.Services
+{
+    public class SnippetSevice : ISnippetSevice
+    {
+        public SnippetSevice()
+        {
+
+        }
+
+        public Task<SnippetPost> CreateAsync(SnippetPost model, CancellationToken ct = default)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(ulong id, CancellationToken ct = default)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<SnippetPost> UpdateAsync(SnippetPost model, CancellationToken ct = default)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
