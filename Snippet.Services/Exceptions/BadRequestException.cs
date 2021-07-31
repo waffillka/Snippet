4db764c48d56718b90c33af0af4358ff04345fc6
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-07-31 21:54:55 +0300

    added BadRequestException

---
+++ Snippet.Services/Exceptions/BadRequestException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Snippet.Services.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string msg) : base(msg)
+        {
+        }
+    }
+}
--- SnippetProject/Middleware/ExceptionMiddlewareExtensions.cs
+++ SnippetProject/Middleware/ExceptionMiddlewareExtensions.cs
@@ -3,6 +3,7 @@ using Entities.ErrorModel;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
+using Snippet.Services.Exceptions;
 using System.Net;
 
 namespace SnippetProject.Middleware
@@ -21,6 +22,17 @@ namespace SnippetProject.Middleware
 
                     if (contextFeature != null)
                     {
+                        if (contextFeature.Error is BadRequestException exception)
+                        {
+                            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                            logger.LogError(exception.Message);
+
+                            await context.Response.WriteAsync(exception.Message).ConfigureAwait(false);
+
+                            return;
+                        }
+
                         logger.LogError($"Something went wrong: {contextFeature.Error}");
                         await context.Response.WriteAsync(new ErrorDetails()
                         {
