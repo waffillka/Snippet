0cf1f1f8728c735a229ea1c0ee713e38bb2cbe01
ZhavoronkovM <zhavoronkov.02@mail.ru>
2021-07-31 13:24:17 +0300

    Add SnippetPostController.

--- SnippetProject/Controllers/SnippetPostController.cs
+++ SnippetProject/Controllers/SnippetPostController.cs
@@ -1,7 +1,24 @@
-﻿namespace SnippetProject.Controllers
+﻿using System;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+using Snippet.Services.Models;
+
+namespace SnippetProject.Controllers
 {
-    public class SnippetPostController
+    [ApiController]
+    [Route("snippet")]
+    public class SnippetPostController : ControllerBase
     {
-        
+        [HttpGet("post/{id}")]
+        public SnippetPost GetPostById(ulong id, CancellationToken ct)
+        {
+            var result = new SnippetPost {Id = id, Description = "Default description", Date = DateTime.Now};
+            return result;
+        }
+
+        // public ShortSnippetPost[] GetShortPosts()
+        // {
+        //     
+        // }
     }
 }
