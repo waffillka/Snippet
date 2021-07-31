db22c216abb10b1a9c85def0069381cecd90fa57
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-07-31 19:40:17 +0300

    create tag provider

---
+++ Snippet.Services/Interfaces/Providers/ITagProvider.cs
@@ -0,0 +1,14 @@
+﻿using Snippet.Services.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snippet.Services.Interfaces.Providers
+{
+    public interface ITagProvider
+    {
+        Task<Tag?> GetByIdAsync(ulong id, CancellationToken ct = default);
+        Task<Tag> CreateAsync(Tag model, CancellationToken ct = default);
+        Task<Tag> UpdateAsync(Tag model, CancellationToken ct = default);
+        Task<bool> DeleteAsync(ulong id, CancellationToken ct = default);
+    }
+}
---
+++ Snippet.Services/Providers/TagProvider.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Snippet.Data.Entities;
+using Snippet.Data.Interfaces.UnitOfWork;
+using Snippet.Services.Interfaces.Providers;
+using Snippet.Services.Models;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snippet.Services.Providers
+{
+    public class TagProvider : ITagProvider
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TagProvider(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Tag> CreateAsync(Tag model, CancellationToken ct = default)
+        {
+            var entity = _mapper.Map<TagEntity>(model);
+            var responseTag = await _unitOfWork.Tags.CreateAsync(entity, ct).ConfigureAwait(false);
+            await _unitOfWork.SaveChangesAsync(ct).ConfigureAwait(false);
+            return _mapper.Map<Tag>(responseTag);
+        }
+
+        public async Task<bool> DeleteAsync(ulong id, CancellationToken ct = default)
+        {
+            var result = await _unitOfWork.Tags.DeleteAsync(id, ct).ConfigureAwait(false);
+            await _unitOfWork.SaveChangesAsync(ct).ConfigureAwait(false);
+
+            return result;
+        }
+
+        public async Task<Tag?> GetByIdAsync(ulong id, CancellationToken ct = default)
+        {
+            var entity = await _unitOfWork.Tags.GetByIdAsync(id, ct).ConfigureAwait(false);
+            return _mapper.Map<Tag?>(entity);
+        }
+
+        public async Task<Tag> UpdateAsync(Tag model, CancellationToken ct = default)
+        {
+            var entity = _mapper.Map<TagEntity>(model);
+
+            var responseEntity = await _unitOfWork.Tags.UpdateAsync(entity, ct).ConfigureAwait(false);
+            await _unitOfWork.SaveChangesAsync(ct).ConfigureAwait(false);
+
+            return _mapper.Map<Tag>(responseEntity);
+        }
+    }
+}
