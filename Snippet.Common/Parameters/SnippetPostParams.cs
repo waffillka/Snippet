f4c960b9c5308501d155415a99b3e569621659ba
ZhavoronkovM <zhavoronkov.02@mail.ru>
2021-07-31 13:52:23 +0300

    Add base base parameters representation. Add SnippetPostParams.

---
+++ Snippet.Common/Parameters/SnippetPostParams.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Snippet.Common.Parameters
+{
+    public class SnippetPostParams : ParamsBase
+    {
+        public ICollection<ulong>? Authors { get; set; }
+        public ICollection<ulong>? AuthorsExclude { get; set; }
+        
+        public ICollection<ulong>? Tags { get; set; }
+        public ICollection<ulong>? TagsExclude { get; set; }
+        
+        public DateTime CreationDate { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        
+        //match something in title, description or snippet
+        public string? MatchString { get; set; }
+    }
+}
