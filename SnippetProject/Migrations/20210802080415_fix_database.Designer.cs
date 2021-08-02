6d3abd4e10770e8bc132053f070e250c01f3cb54
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-08-02 11:17:45 +0300

    update migration files

--- SnippetProject/Migrations/20210802080415_fix_database.Designer.cs
+++ SnippetProject/Migrations/20210802080415_fix_database.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Snippet.Data.DbContext;
 
 namespace SnippetProject.Migrations
 {
     [DbContext(typeof(SnippetDbContext))]
-    partial class SnippetDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210802080415_fix_database")]
+    partial class fix_database
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
