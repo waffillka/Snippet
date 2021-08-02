e5727e03ec436e5eae151a54633949814be870be
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-08-02 18:17:56 +0300

    update migrations files

--- SnippetProject/Migrations/20210802122943_fix_database_v_2.Designer.cs
+++ SnippetProject/Migrations/20210802122943_fix_database_v_2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Snippet.Data.DbContext;
 
 namespace SnippetProject.Migrations
 {
     [DbContext(typeof(SnippetDbContext))]
-    partial class SnippetDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210802122943_fix_database_v_2")]
+    partial class fix_database_v_2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SnippetProject/Migrations/20210802122943_fix_database_v_2.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SnippetProject.Migrations
+{
+    public partial class fix_database_v_2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SnippetPosts_Languages_LanguageEntityId",
+                table: "SnippetPosts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SnippetPosts_LanguageEntityId",
+                table: "SnippetPosts");
+
+            migrationBuilder.DropColumn(
+                name: "LanguageEntityId",
+                table: "SnippetPosts");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SnippetPosts_LanguageId",
+                table: "SnippetPosts",
+                column: "LanguageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SnippetPosts_UserId",
+                table: "SnippetPosts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SnippetPosts_Languages_LanguageId",
+                table: "SnippetPosts",
+                column: "LanguageId",
+                principalTable: "Languages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SnippetPosts_Users_UserId",
+                table: "SnippetPosts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SnippetPosts_Languages_LanguageId",
+                table: "SnippetPosts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SnippetPosts_Users_UserId",
+                table: "SnippetPosts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SnippetPosts_LanguageId",
+                table: "SnippetPosts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SnippetPosts_UserId",
+                table: "SnippetPosts");
+
+            migrationBuilder.AddColumn<long>(
+                name: "LanguageEntityId",
+                table: "SnippetPosts",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SnippetPosts_LanguageEntityId",
+                table: "SnippetPosts",
+                column: "LanguageEntityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SnippetPosts_Languages_LanguageEntityId",
+                table: "SnippetPosts",
+                column: "LanguageEntityId",
+                principalTable: "Languages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
