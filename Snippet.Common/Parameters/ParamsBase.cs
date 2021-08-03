5937960ad6180c0f914c40a45189728c56cb62fc
ZhavoronkovM <zhavoronkov.02@mail.ru>
2021-08-03 22:17:12 +0300

    Configure default values for SnippetPostParams and ParamsBase.

--- Snippet.Common/Parameters/ParamsBase.cs
+++ Snippet.Common/Parameters/ParamsBase.cs
@@ -4,7 +4,7 @@ namespace Snippet.Common.Parameters
 {
     public class ParamsBase
     {
-        public int Page { get; set; } = 0;
+        public int Page { get; set; } = 1;
         public int PageSize { get; set; } = 20;
         public string OrderBy { get; set; } = string.Empty;
         public OrderDirection OrderDirection { get; set; } = OrderDirection.Asc;
--- Snippet.Common/Parameters/SnippetPostParams.cs
+++ Snippet.Common/Parameters/SnippetPostParams.cs
@@ -5,17 +5,17 @@ namespace Snippet.Common.Parameters
 {
     public class SnippetPostParams : ParamsBase
     {
-        public ICollection<long>? Authors { get; set; }
-        public ICollection<long>? AuthorsExclude { get; set; }
+        public ICollection<long>? Authors { get; set; } = default;
+        public ICollection<long>? AuthorsExclude { get; set; } = default;
 
-        public ICollection<long>? Tags { get; set; }
-        public ICollection<long>? TagsExclude { get; set; }
+        public ICollection<long>? Tags { get; set; } = default;
+        public ICollection<long>? TagsExclude { get; set; } = default;
 
-        public DateTime CreationDate { get; set; }
-        public DateTime From { get; set; }
-        public DateTime To { get; set; }
+        public DateTime CreationDate { get; set; } = default;
+        public DateTime From { get; set; } = default;
+        public DateTime To { get; set; } = default;
 
         //match something in title, description or snippet
-        public string? MatchString { get; set; }
+        public string? MatchString { get; set; } = string.Empty;
     }
 }
