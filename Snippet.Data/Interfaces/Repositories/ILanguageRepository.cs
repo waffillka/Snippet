d7ae24fed325ec81d84fff16b262f54a90ce02ce
ZhavoronkovM <zhavoronkov.02@mail.ru>
2021-08-03 18:03:41 +0300

    Add lag filtration and sort.

--- Snippet.Data/Interfaces/Repositories/ILanguageRepository.cs
+++ Snippet.Data/Interfaces/Repositories/ILanguageRepository.cs
@@ -1,12 +1,16 @@
-﻿using Snippet.Data.Entities;
+﻿using System.Collections.Generic;
+using Snippet.Data.Entities;
 using System.Threading;
 using System.Threading.Tasks;
+using Snippet.Common.Parameters;
 
 namespace Snippet.Data.Interfaces.Repositories
 {
     public interface ILanguageRepository
     {
         Task<LanguageEntity?> GetByIdAsync(long id, CancellationToken ct = default);
+        Task<LanguageEntity?> GetByNameAsync(string name, CancellationToken ct = default);
+        Task<IEnumerable<LanguageEntity>> GetAllAsync(ParamsBase? parameters = default, CancellationToken ct = default);
         Task<LanguageEntity> CreateAsync(LanguageEntity entity, CancellationToken ct = default);
         Task<LanguageEntity> UpdateAsync(LanguageEntity entity, CancellationToken ct = default);
         Task<bool> DeleteAsync(long id, CancellationToken ct = default);
