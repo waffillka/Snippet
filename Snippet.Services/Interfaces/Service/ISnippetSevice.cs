e5d905efbf686f08aaa169fb81aa936aec63a356
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-08-04 22:30:14 +0300

    Implemented snippet service

--- Snippet.Services/Interfaces/Service/ISnippetSevice.cs
+++ Snippet.Services/Interfaces/Service/ISnippetSevice.cs
@@ -1,6 +1,8 @@
-﻿using Snippet.Services.Models;
+﻿using Snippet.Common.Parameters;
+using Snippet.Services.Models;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Snippet.Services.Interfaces.Service
 {
@@ -9,5 +11,7 @@ namespace Snippet.Services.Interfaces.Service
         Task<SnippetPost> CreateAsync(SnippetPost model, CancellationToken ct = default);
         Task<SnippetPost> UpdateAsync(SnippetPost model, CancellationToken ct = default);
         Task<bool> DeleteAsync(long id, CancellationToken ct = default);
+        Task<SnippetPost?> GetByIdAsync(long id, CancellationToken ct = default);
+        Task<IReadOnlyCollection<ShortSnippetPost>> GetAllShortAsync(SnippetPostParams? parameters = default, CancellationToken ct = default);
     }
 }
--- Snippet.Services/Services/SnippetSevice.cs
+++ Snippet.Services/Services/SnippetSevice.cs
@@ -1,5 +1,8 @@
-﻿using Snippet.Services.Interfaces.Service;
+﻿using Snippet.Common.Parameters;
+using Snippet.Services.Interfaces.Providers;
+using Snippet.Services.Interfaces.Service;
 using Snippet.Services.Models;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,24 +10,48 @@ namespace Snippet.Services.Services
 {
     public class SnippetSevice : ISnippetSevice
     {
-        public SnippetSevice()
+        private readonly ISnippetProvider _snippetProvider;
+        public SnippetSevice(ISnippetProvider snippetProvider)
         {
-
+            _snippetProvider = snippetProvider;
         }
 
         public Task<SnippetPost> CreateAsync(SnippetPost model, CancellationToken ct = default)
         {
-            throw new System.NotImplementedException();
+            return _snippetProvider.CreateAsync(model, ct);
         }
 
         public Task<bool> DeleteAsync(long id, CancellationToken ct = default)
         {
-            throw new System.NotImplementedException();
+            return _snippetProvider.DeleteAsync(id, ct);
+        }
+
+        public async Task<IReadOnlyCollection<ShortSnippetPost>> GetAllShortAsync(SnippetPostParams? parameters = null, CancellationToken ct = default)
+        {
+            var shortPosts = await _snippetProvider.GetAllShortAsync(parameters, ct).ConfigureAwait(false);
+
+            foreach(var item in shortPosts)
+            {
+                item.Like = await _snippetProvider.CountLike(item.Id, ct).ConfigureAwait(false);
+            }
+
+            return shortPosts;
+        }
+
+        public async Task<SnippetPost?> GetByIdAsync(long id, CancellationToken ct = default)
+        {
+            var snippetModel = await _snippetProvider.GetByIdAsync(id, ct).ConfigureAwait(false);
+            if (snippetModel == null)
+            {
+                throw new System.NotImplementedException();
+            }
+            snippetModel.Like = await _snippetProvider.CountLike(id, ct).ConfigureAwait(false);
+            return snippetModel;
         }
 
         public Task<SnippetPost> UpdateAsync(SnippetPost model, CancellationToken ct = default)
         {
-            throw new System.NotImplementedException();
+            return _snippetProvider.CreateAsync(model, ct);
         }
     }
 }
