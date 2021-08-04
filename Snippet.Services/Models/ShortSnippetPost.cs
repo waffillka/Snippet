ed1020f10ed484efc4890e0923f2d44f2b3c3105
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-08-04 21:06:17 +0300

    add ShortSnippetPost

---
+++ Snippet.Services/Models/ShortSnippetPost.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snippet.Services.Models
+{
+    public class ShortSnippetPost
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public Language Language { get; set; }
+
+        public User User { get; set; }
+
+        public int Like { get; set; }
+        public ICollection<Tag>? Tags { get; set; }
+    }
+}
