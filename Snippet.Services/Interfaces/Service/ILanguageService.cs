82c762b77a2c60abde12c79b52329cbb20ffc800
ZhavoronkovM <zhavoronkov.02@mail.ru>
2021-08-05 10:59:27 +0300

    Create LanguageService.

---
+++ Snippet.Services/Interfaces/Service/ILanguageService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Snippet.Services.Models;
+
+namespace Snippet.Services.Interfaces.Service
+{
+    public interface ILanguageService
+    {
+        Task<Language?> GetByIdAsync(long id, CancellationToken ct = default);
+        Task<Language> CreateAsync(Language model, CancellationToken ct = default);
+        Task<Language> UpdateAsync(Language model, CancellationToken ct = default);
+        Task<bool> DeleteAsync(long id, CancellationToken ct = default);
+    }
+}
---
+++ Snippet.Services/Services/LanguageService.cs
@@ -0,0 +1,14 @@
+﻿using Snippet.Services.Providers;
+
+namespace Snippet.Services.Services
+{
+    public class LanguageService
+    {
+        private LanguageProvider _provider;
+
+        public LanguageService(LanguageProvider provider)
+        {
+            _provider = provider;
+        }
+    }
+}
