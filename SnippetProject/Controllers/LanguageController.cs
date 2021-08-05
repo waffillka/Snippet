f423151ed2c952ad07db9a59493ae00b68f9cff8
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-08-05 20:45:27 +0300

    Implemented request GetAll in language controller

--- SnippetProject/Controllers/LanguageController.cs
+++ SnippetProject/Controllers/LanguageController.cs
@@ -1,9 +1,11 @@
 ﻿#nullable enable
 using Microsoft.AspNetCore.Mvc;
 using Snippet.Common.Parameters;
+using Snippet.Services.Interfaces.Service;
 using Snippet.Services.Models;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace SnippetProject.Controllers
 {
@@ -11,18 +13,19 @@ namespace SnippetProject.Controllers
     [Route("lang")]
     public class LanguageController : ControllerBase
     {
+        private readonly ILanguageService _languageService;
+
+        public LanguageController(ILanguageService languageService)
+        {
+            _languageService = languageService;
+        }
+
         [HttpGet("many")]
-        public Language[] GetAll([FromQuery] ParamsBase? parameters, CancellationToken ct)
+        public async Task<IActionResult> GetAll([FromQuery] ParamsBase? parameters, CancellationToken ct)
         {
             parameters ??= new ParamsBase();
-
-            var result = new Language[parameters.PageSize].Select((x, index) =>
-            {
-                x = new Language { Id = index, Name = "Haskell" };
-                return x;
-            });
-
-            return result.ToArray();
+            var result = await _languageService.GetAllAsync(parameters, ct).ConfigureAwait(false);
+            return Ok(result);
         }
 
         [HttpGet("most-popular")]
