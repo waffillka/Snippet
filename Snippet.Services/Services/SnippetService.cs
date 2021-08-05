53ed2ee293820f0ac77a35f781219d1468e72852
ZhavoronkovM <zhavoronkov.02@mail.ru>
2021-08-05 21:38:37 +0300

    Change async methods of SnippetService to return started tasks of provider.

--- Snippet.Services/Services/SnippetService.cs
+++ Snippet.Services/Services/SnippetService.cs
@@ -5,25 +5,20 @@ using Snippet.Services.Models;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Snippet.Services.Parser;
 
 namespace Snippet.Services.Services
 {
     public class SnippetService : ISnippetService
     {
         private readonly ISnippetProvider _snippetProvider;
-        private readonly ITagParser _parser;
-        public SnippetService(ISnippetProvider snippetProvider, ITagParser parser)
+        public SnippetService(ISnippetProvider snippetProvider)
         {
             _snippetProvider = snippetProvider;
-            _parser = parser;
         }
 
-        public async Task<SnippetPost> CreateAsync(SnippetPost model, CancellationToken ct = default)
+        public Task<SnippetPost> CreateAsync(SnippetPost model, CancellationToken ct = default)
         {
-            var tags = await _parser.GetTags(model.Snippet, ct).ConfigureAwait(false);
-            model.Tags = tags;
-            return await _snippetProvider.CreateAsync(model, ct).ConfigureAwait(false);
+            return _snippetProvider.CreateAsync(model, ct);
         }
 
         public Task<bool> DeleteAsync(long id, CancellationToken ct = default)
@@ -31,44 +26,29 @@ namespace Snippet.Services.Services
             return _snippetProvider.DeleteAsync(id, ct);
         }
 
-        public async Task<IReadOnlyCollection<SnippetPost>> GetAllAsync(SnippetPostParams? parameters = null, CancellationToken ct = default)
+        public Task<IReadOnlyCollection<SnippetPost>> GetAllAsync(SnippetPostParams? parameters = null, CancellationToken ct = default)
         {
-            var posts = await _snippetProvider.GetAllAsync(parameters, ct).ConfigureAwait(false);
-
-            return posts;
+            return _snippetProvider.GetAllAsync(parameters, ct);
         }
 
-        public async Task<IReadOnlyCollection<ShortSnippetPost>> GetAllShortAsync(SnippetPostParams? parameters = null, CancellationToken ct = default)
+        public Task<IReadOnlyCollection<ShortSnippetPost>> GetAllShortAsync(SnippetPostParams? parameters = null, CancellationToken ct = default)
         {
-            var shortPosts = await _snippetProvider.GetAllShortAsync(parameters, ct).ConfigureAwait(false);
-
-            return shortPosts;
+            return _snippetProvider.GetAllShortAsync(parameters, ct);
         }
 
-        public async Task<SnippetPost?> GetByIdAsync(long id, CancellationToken ct = default)
+        public Task<SnippetPost?> GetByIdAsync(long id, CancellationToken ct = default)
         {
-            var snippetModel = await _snippetProvider.GetByIdAsync(id, ct).ConfigureAwait(false);
-            if (snippetModel == null)
-            {
-                throw new System.NotImplementedException();
-            }
-            return snippetModel;
+            return _snippetProvider.GetByIdAsync(id, ct);
         }
 
-        public async Task<ShortSnippetPost?> GetShortPostById(long id, CancellationToken ct = default)
+        public Task<ShortSnippetPost?> GetShortPostById(long id, CancellationToken ct = default)
         {
-            var snippetShortModel = await _snippetProvider.GetShortPostById(id, ct).ConfigureAwait(false);
-            if (snippetShortModel == null)
-            {
-                throw new System.NotImplementedException();
-            }
-
-            return snippetShortModel;
+            return _snippetProvider.GetShortPostById(id, ct);
         }
 
         public Task<SnippetPost> UpdateAsync(SnippetPost model, CancellationToken ct = default)
         {
-            return _snippetProvider.CreateAsync(model, ct);
+            return _snippetProvider.UpdateAsync(model, ct);
         }
     }
 }
