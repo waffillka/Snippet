faa1a3d0be9b451617fac879d01abb845cc39d09
ZhavoronkovM <zhavoronkov.02@mail.ru>
2021-08-05 21:37:01 +0300

    Add invalid values checks.

--- Snippet.Services/Providers/SnippetProvider.cs
+++ Snippet.Services/Providers/SnippetProvider.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System;
+using AutoMapper;
 using Snippet.Common.Parameters;
 using Snippet.Data.Entities;
 using Snippet.Data.Interfaces.UnitOfWork;
@@ -7,6 +8,8 @@ using Snippet.Services.Models;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Snippet.Common.Exceptions;
+using Snippet.Services.Parser;
 
 namespace Snippet.Services.Providers
 {
@@ -14,31 +17,45 @@ namespace Snippet.Services.Providers
     {
         private readonly IMapper _mapper;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly ITagParser _parser;
 
-        public SnippetProvider(IMapper mapper, IUnitOfWork unitOfWork)
+        public SnippetProvider(IMapper mapper, IUnitOfWork unitOfWork, ITagParser parser)
         {
             _mapper = mapper;
             _unitOfWork = unitOfWork;
+            _parser = parser;
         }
 
-        public Task<int> CountLike(long id, CancellationToken ct = default)
+        public async Task<int> CountLike(long id, CancellationToken ct = default)
         {
-            return _unitOfWork.Snippets.CountLike(id, ct);
+            if (await GetByIdAsync(id, ct).ConfigureAwait(false) == null)
+                throw new UserNotFoundException("User with specified id does not exist.");
+            
+            return await _unitOfWork.Snippets.CountLike(id, ct).ConfigureAwait(false);
         }
 
-        public async Task<SnippetPost> CreateAsync(SnippetPost model, CancellationToken ct = default)
+        public async Task<SnippetPost> CreateAsync(SnippetPost? model, CancellationToken ct = default)
         {
+            if (model == null)
+                throw new ArgumentNullException(nameof(model));
+            
+            var tags = await _parser.GetTags(model.Snippet, ct).ConfigureAwait(false);
+            model.Tags = tags;
+            
             var entity = _mapper.Map<SnippetEntity>(model);
-
-            var responseEntity = await _unitOfWork.Snippets.CreateAsync(entity, ct).ConfigureAwait(false);
+            entity.Date = DateTime.Now;
+            
+            var createdEntity = await _unitOfWork.Snippets.CreateAsync(entity, ct).ConfigureAwait(false);
             await _unitOfWork.SaveChangesAsync(ct).ConfigureAwait(false);
 
-            return _mapper.Map<SnippetPost>(responseEntity);
-
+            return _mapper.Map<SnippetPost>(createdEntity);
         }
 
         public async Task<bool> DeleteAsync(long id, CancellationToken ct = default)
         {
+            if (await GetByIdAsync(id, ct).ConfigureAwait(false) == null)
+                throw new UserNotFoundException("User with specified id does not exist.");
+            
             var result = await _unitOfWork.Snippets.DeleteAsync(id, ct).ConfigureAwait(false);
             await _unitOfWork.SaveChangesAsync(ct).ConfigureAwait(false);
 
@@ -47,7 +64,9 @@ namespace Snippet.Services.Providers
 
         public async Task<IReadOnlyCollection<SnippetPost>> GetAllAsync(SnippetPostParams? parameters = null, CancellationToken ct = default)
         {
-            var entities = await _unitOfWork.Snippets.GetAllAsync(parameters, ct).ConfigureAwait(false);
+            var entities = await _unitOfWork.Snippets
+                .GetAllAsync(parameters, ct)
+                .ConfigureAwait(false);
 
             var result = _mapper.Map<IReadOnlyCollection<SnippetPost>>(entities);
 
@@ -59,9 +78,12 @@ namespace Snippet.Services.Providers
             return result;
         }
 
-        public async Task<IReadOnlyCollection<ShortSnippetPost>> GetAllShortAsync(SnippetPostParams? parameters = default, CancellationToken ct = default)
+        public async Task<IReadOnlyCollection<ShortSnippetPost>> GetAllShortAsync
+            (SnippetPostParams? parameters = default, CancellationToken ct = default)
         {
-            var entities = await _unitOfWork.Snippets.GetAllAsync(parameters, ct).ConfigureAwait(false);
+            var entities = await _unitOfWork.Snippets
+                .GetAllAsync(parameters, ct)
+                .ConfigureAwait(false);
 
             var result = _mapper.Map<IReadOnlyCollection<ShortSnippetPost>>(entities);
 
@@ -85,8 +107,14 @@ namespace Snippet.Services.Providers
             return _mapper.Map<ShortSnippetPost?>(entity);
         }
 
-        public async Task<SnippetPost> UpdateAsync(SnippetPost model, CancellationToken ct = default)
+        public async Task<SnippetPost> UpdateAsync(SnippetPost? model, CancellationToken ct = default)
         {
+            if (model == null)
+                throw new ArgumentNullException(nameof(model));
+            
+            if (await GetByIdAsync(model.Id, ct).ConfigureAwait(false) == null)
+                throw new UserNotFoundException("User with specified id does not exist.");
+            
             var entity = _mapper.Map<SnippetEntity>(model);
 
             var responseEntity = _unitOfWork.Snippets.Update(entity, ct);
