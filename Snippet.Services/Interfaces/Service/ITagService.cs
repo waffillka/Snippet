428ce9ff28c4bbeb7244b850c0e3f0865a4706e0
ZhavoronkovM <zhavoronkov.02@mail.ru>
2021-08-05 15:41:02 +0300

    Implement TagService.

---
+++ Snippet.Services/Interfaces/Service/ITagService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Snippet.Common.Parameters;
+using Snippet.Services.Models;
+
+namespace Snippet.Services.Interfaces.Service
+{
+    public interface ITagService
+    {
+        Task<Tag?> GetByIdAsync(long id, CancellationToken ct = default);
+        Task<IEnumerable<Tag>> GetAllAsync(ParamsBase? parameters, CancellationToken ct = default);
+        Task<Tag?> GetByNameAsync(string name, CancellationToken ct = default);
+        Task<Tag> CreateAsync(Tag model, CancellationToken ct = default);
+        Task<Tag> UpdateAsync(Tag model, CancellationToken ct = default);
+        Task<bool> DeleteAsync(long id, CancellationToken ct = default);
+    }
+}
---
+++ Snippet.Services/Services/TagService.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Snippet.Common.Parameters;
+using Snippet.Services.Interfaces.Providers;
+using Snippet.Services.Interfaces.Service;
+using Snippet.Services.Models;
+
+namespace Snippet.Services.Services
+{
+    public class TagService : ITagService
+    {
+        private readonly ITagProvider _provider;
+
+
+        public TagService(ITagProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public Task<Tag?> GetByIdAsync(long id, CancellationToken ct = default)
+        {
+            return _provider.GetByIdAsync(id, ct);
+        }
+
+        public Task<IEnumerable<Tag>> GetAllAsync(ParamsBase? parameters, CancellationToken ct = default)
+        {
+            return _provider.GetAllAsync(parameters, ct);
+        }
+
+        public Task<Tag?> GetByNameAsync(string name, CancellationToken ct = default)
+        {
+            return _provider.GetByNameAsync(name, ct);
+        }
+
+        public Task<Tag> CreateAsync(Tag model, CancellationToken ct = default)
+        {
+            return _provider.CreateAsync(model, ct);
+        }
+
+        public Task<Tag> UpdateAsync(Tag model, CancellationToken ct = default)
+        {
+            return _provider.UpdateAsync(model, ct);
+        }
+
+        public Task<bool> DeleteAsync(long id, CancellationToken ct = default)
+        {
+            return _provider.DeleteAsync(id, ct);
+        }
+    }
+}
