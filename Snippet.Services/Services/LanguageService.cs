e9ee939a1f3621a6c34befb21cfa74528c30231f
ZhavoronkovM <zhavoronkov.02@mail.ru>
2021-08-05 11:10:32 +0300

    Add LanguageService methods.

--- Snippet.Services/Services/LanguageService.cs
+++ Snippet.Services/Services/LanguageService.cs
@@ -1,14 +1,38 @@
-﻿using Snippet.Services.Providers;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Snippet.Services.Interfaces.Service;
+using Snippet.Services.Models;
+using Snippet.Services.Providers;
 
 namespace Snippet.Services.Services
 {
-    public class LanguageService
+    public class LanguageService : ILanguageService
     {
-        private LanguageProvider _provider;
+        private readonly LanguageProvider _provider;
 
         public LanguageService(LanguageProvider provider)
         {
             _provider = provider;
+        }
+
+        public Task<Language?> GetByIdAsync(long id, CancellationToken ct = default)
+        {
+            return _provider.GetByIdAsync(id, ct);
+        }
+
+        public Task<Language> CreateAsync(Language model, CancellationToken ct = default)
+        {
+            return _provider.CreateAsync(model, ct);
+        }
+
+        public Task<Language> UpdateAsync(Language model, CancellationToken ct = default)
+        {
+            return _provider.UpdateAsync(model, ct);
+        }
+
+        public Task<bool> DeleteAsync(long id, CancellationToken ct = default)
+        {
+            return _provider.DeleteAsync(id, ct);
         }
     }
 }
