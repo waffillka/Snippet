d956e8b1d5123f100438e5df948441d9a12486ec
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-08-06 10:36:15 +0300

    create response model

--- Snippet.Services/Response/SnippetPostResponse.cs
+++ Snippet.Services/Response/SnippetPostResponse.cs
@@ -1,6 +1,34 @@
-﻿namespace Snippet.Services.Response
+﻿using Snippet.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Snippet.Services.Response
 {
-    class SnippetPostResponse
+    public  class SnippetPostResponse
     {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "Title is a required field.")]
+        [MaxLength(140, ErrorMessage = "Maximum length for the Title is 140 characters.")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Description is a required field.")]
+        [MaxLength(2000, ErrorMessage = "Maximum length for the Description is 2000 characters.")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Snippet is a required field.")]
+        [MaxLength(4000, ErrorMessage = "Maximum length for the Snippet is 4000 characters.")]
+        public string Snippet { get; set; } = string.Empty;
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "LanguageId is a required field.")]
+        public Language? Language { get; set; }
+
+        [Required(ErrorMessage = "UserId is a required field.")]
+        public User? User { get; set; }
+        public int Like { get; set; }
+        public ICollection<Tag> Tags { get; set; }
     }
 }
