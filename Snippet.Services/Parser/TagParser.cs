0008308a40681e5977fc20f9391b3ea6b9b2f844
ZhavoronkovM <zhavoronkov.02@mail.ru>
2021-08-07 21:15:08 +0300

    Change TagParser implementation.

--- Snippet.Services/Parser/TagParser.cs
+++ Snippet.Services/Parser/TagParser.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Snippet.Services.Parser
 {
@@ -12,22 +11,9 @@ namespace Snippet.Services.Parser
     {
         private const string Pattern = @"#\w+";
 
-        private readonly ITagProvider _provider;
-
-        public TagParser(ITagProvider provider)
+        public IReadOnlyCollection<string> ParseTags(string data, CancellationToken ct = default)
         {
-            _provider = provider;
-        }
-
-        public async Task<ICollection<Tag>> GetTags(string data, CancellationToken ct = default)
-        {
-            var matches = Regex.Matches(data, Pattern);
-            
-            var result = await _provider
-                .GetByNamesAsync(matches.Select(match => match.Value), ct)
-                .ConfigureAwait(false);
-            
-            return result;
+            return Regex.Matches(data, Pattern).Select(match => match.Value).ToList();
         }
     }
 }
