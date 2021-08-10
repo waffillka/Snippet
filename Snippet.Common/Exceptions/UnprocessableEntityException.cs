5e52e1c819b6792fe96d4feca8954ebd2afe35d1
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-08-11 01:38:17 +0300

    add custom exception

---
+++ Snippet.Common/Exceptions/UnprocessableEntityException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snippet.Common.Exceptions
+{
+    public class UnprocessableEntityException : Exception
+    {
+        public UnprocessableEntityException(string message) : base(message)
+        {
+        }
+    }
+}
