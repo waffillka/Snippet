f87ea3a777de9e6c6fd1c8920beb0c2f18620449
George Sebik <43186989+waffillka@users.noreply.github.com>
2021-08-10 14:37:50 +0300

    Added DbConnection health check.

---
+++ SnippetProject/HealthChecks/DbConnectionHealthCheck.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SnippetProject.HealthChecks
+{
+    public class DbConnectionHealthCheck : IHealthCheck
+    {
+        public string TestQuery { get; }
+        public string ConnectionString { get; }
+
+        public DbConnectionHealthCheck(string connectionString, string testQuery = "Select 1;")
+        {
+            ConnectionString = connectionString ?? throw new ArgumentNullException("Connection string is null");
+            TestQuery = testQuery;
+        }
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken ct = default)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync(ct).ConfigureAwait(false);
+
+                    var command = connection.CreateCommand();
+                    command.CommandText = TestQuery;
+                    await command.ExecuteNonQueryAsync(ct).ConfigureAwait(false);
+                }
+                catch (DbException ex)
+                {
+                    return new HealthCheckResult(status: context.Registration.FailureStatus, exception: ex);
+                }
+            }
+
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
